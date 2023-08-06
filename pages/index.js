e0699c9f77e6883002e77abcdbf51effc6182e60
Maique Rosa <maique.rosa@gmail.com>
2023-08-05 21:30:36 -0300

    teste

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main>
         <h1 className={styles.title}>
-          Bem vindo a <a href="https://nextjs.org">Maique Application!</a>
+          Alterando o valor de <a href="https://nextjs.org">Maique Application!</a>
         </h1>
 
         <p className={styles.description}>
